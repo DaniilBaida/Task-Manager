38c3065687a14302d14a58f8cf1f877f70a0b757
Daniil Baida <devbaida78@gmail.com>
2025-08-25 21:13:37 +0100

    Integrate winston for logging and add morgan middleware

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,7 @@ const config = {
     NODE_ENV: process.env.NODE_ENV || "development",
     PORT: parseInt(process.env.PORT || "3000"),
     APP_DEBUG: process.env.APP_DEBUG === "true",
+    LOG_LEVEL: process.env.LOG_LEVEL || "info",
     APP_SECRET: process.env.APP_SECRET || "",
     ISSUER_BASE_URL: process.env.ISSUER_BASE_URL || "",
     AUDIENCE: process.env.AUDIENCE || "",
