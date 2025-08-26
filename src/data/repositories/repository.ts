4f147df69e8e1cd11e0d90457d06289e0fb9ca37
Daniil Baida <devbaida78@gmail.com>
2025-08-26 23:06:49 +0100

    Add repository interfaces for tasks and projects

---
+++ src/data/repositories/repository.ts
@@ -0,0 +1,59 @@
+export interface ITask {
+    id: string;
+    user_id: string;
+    project_id: string | null;
+    name: string;
+    description: string | null;
+    due_date: Date | null;
+    completed_on: Date | null;
+    created_at: Date;
+}
+
+export interface IProject {
+    id: string;
+    user_id: string;
+    name: string;
+    description: string | null;
+    created_at: Date;
+}
+
+interface IQueryParameters {
+    limit?: number;
+    offset?: number;
+}
+
+export interface ITaskQueryParameters extends IQueryParameters {
+    projectId?: string;
+}
+
+export interface IProjectQueryParameters extends IQueryParameters {}
+
+export interface ITaskRepository {
+    getAllTasks(query: ITaskQueryParameters, userId: string): Promise<ITask[]>;
+    getTask(id: string, userId: string): Promise<ITask>;
+    createTask(payload: Partial<ITask>, userId: string): Promise<ITask>;
+    updateTask(
+        id: string,
+        payload: Partial<ITask>,
+        userId: string
+    ): Promise<ITask>;
+}
+
+export interface IProjectRepository {
+    getAllProjects(
+        query: IProjectQueryParameters,
+        userId: string
+    ): Promise<IProject[]>;
+    getProject(id: string, userId: string): Promise<IProject>;
+    getAllProjectTasks(id: string, userId: string): Promise<ITask>;
+    createProject(
+        payload: Partial<IProject>,
+        id: string,
+        userId: string
+    ): Promise<IProject>;
+    updateProject(
+        id: string,
+        payload: Partial<IProject>,
+        userId: string
+    ): Promise<IProject>;
+}
